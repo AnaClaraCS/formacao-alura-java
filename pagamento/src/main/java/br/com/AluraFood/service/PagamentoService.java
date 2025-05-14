eda56d4df5d3e2951ffc3f624ffd936cf0aac2d0
AnaClaraCS <anaclaracostasouza@yahoo.com.br>
2025-05-14 13:08:31 -0300

    Correçao de import

--- pagamento/src/main/java/br/com/AluraFood/service/PagamentoService.java
+++ pagamento/src/main/java/br/com/AluraFood/service/PagamentoService.java
@@ -1,6 +1,6 @@
 package br.com.AluraFood.service;
 
-import org.hibernate.query.Page;
+import org.springframework.data.domain.Page;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Pageable;
